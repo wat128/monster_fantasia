75e572561ea959674b3d56fb3342f9fdf47077ac
wat <wat128asp@gmail.com>
2020-12-17 19:45:01 +0900

    initial commit

---
+++ app/src/main/java/com/wat128/monsterfantasia/MainActivity.java
@@ -0,0 +1,14 @@
+package com.wat128.monsterfantasia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
